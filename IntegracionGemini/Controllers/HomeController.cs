5c718c46113b16c4ce5b27c4347c50e378f8b5c1
Mathias <mathias.david.ruales@gmail.com>
2025-05-15 08:56:45 -0500

    aaaaaaaaa

--- IntegracionGemini/Controllers/HomeController.cs
+++ IntegracionGemini/Controllers/HomeController.cs
@@ -33,6 +33,6 @@ namespace IntegracionGemini.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
-    
+    //aaaaaaaaaaaaa
 
 }
